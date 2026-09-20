a731456701b415ca5b6992f2d05be0a8fa5cc149
Navaneeth Krishna S <navaneethjithu10@gmail.com>
2026-09-20 14:11:44 +0530

    Remove duplicate Navbar from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/home/Hero";
 import FeaturedRetreats from "@/components/home/featured/FeaturedRetreats";
 import Experiences from "@/components/home/experiences/Experiences";
@@ -7,7 +6,6 @@ import SignatureStays from "@/components/home/stays/SignatureStays";
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <FeaturedRetreats />
       <Experiences />
